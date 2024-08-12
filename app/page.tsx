4815c60de669546e02dd11740c0607a6af9e200a
Dylan Sanders <sandersd27@gmail.com>
2024-08-12 13:02:39 -0400

    init telegram

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
+import { useEffect } from "react";
+import WebApp from '@twa-dev/sdk'
 
 export default function Home() {
+  useEffect(() => {
+    WebApp.ready();
+  }, []);
+
+  const closeApp = () => {
+    WebApp.close();
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
